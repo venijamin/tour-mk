d50adc22a1711c7477517bb3c98310cfa2102ba5
Borjan Peovski <51805234+borjan2@users.noreply.github.com>
2023-02-11 17:54:44 +0100

    Removed comments from javascript files

--- Domasna 4/diansProekt2/src/main/resources/static/placesScript.js
+++ Domasna 4/diansProekt2/src/main/resources/static/placesScript.js
@@ -13,18 +13,3 @@ function toggleDarkMode() {
     currentMode = "light";
   }
 }
-
-// let darkModeButton = document.getElementById(".dark-mode-button");
-
-// $(darkModeButton).click({
-//   function(currentMode) {
-//     if (currentMode == "light") {
-//       document.body.classList.toggle("dark-mode-body");
-//       document.querySelectorAll("ul li a").toggle("link-dark-mdoe");
-//       currentMode = "dark";
-//     } else if (currentMode == "dark") {
-//       document.body.classList.toggle("dark-mode-body");
-//       currentMode = "light";
-//     }
-//   },
-// });
